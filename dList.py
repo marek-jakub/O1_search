78b3f2f5f5a7d50b9e023ae6f381d7592272849c
marek-jakub <marekjakub@hotmail.co.uk>
2021-12-03 19:10:13 +0000

    Define is_empty method for class DList.

--- dList.py
+++ dList.py
@@ -6,3 +6,11 @@ class DList:
         """ Initial object is an empty list."""
         self.level = 61
         self.dList = [None for _ in range(self.level)]
+
+    def is_empty(self):
+        """ Returns true if and only if the list 'dList' is empty. """
+        is_empty = True
+        for i in self.dList:
+            if i is not None:
+                is_empty = False
+        return is_empty
