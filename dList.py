612665fe17b30426c4f8d12902f32597952e8768
marek-jakub <marekjakub@hotmail.co.uk>
2021-12-03 18:50:49 +0000

    Define __init__ method for class DList.

---
+++ dList.py
@@ -0,0 +1,8 @@
+# Marek Jakub
+# 2021
+
+class DList:
+    def __init__(self):
+        """ Initial object is an empty list."""
+        self.level = 61
+        self.dList = [None for _ in range(self.level)]
